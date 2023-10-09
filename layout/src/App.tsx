c4443251bd3c80d0d9588647a8e55ef96a01f3ee
Lucas <lucas.dias@duque.g12.br>
2023-10-09 20:34:58 -0300

    Quarto commit

--- layout/src/App.tsx
+++ layout/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
-    <h1>Lucas Thierry</h1>
-  )
+    <div className="w-30 h-30 bg-red-800 m-5 rounded-x1 border-4 border-red-600 shadow-red-900 shadow-lg">
+      <p className="text-white text-center text-3xl shadow-lg p-2 ">Lucas Thierry</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
